f15388438dca34a551e80cd7608dc6a32937b85c
dracen <a.hayashi0113@gmail.com>
2018-05-05 23:11:35 +0900

    RCサーボ リードスイッチ PSコントローラ

--- smart_dana/smart_dana.ino
+++ smart_dana/smart_dana.ino
@@ -6,6 +6,15 @@
 #define PS2_CLK        13  //PD13(SCK)
 #define PS2_SEL        10  //PD10(SS)
 
+#define LEAD_SW      5  //PD8(LEAD_SW)
+
+#define SERVO_SIGNAL      9  //PD9(SERVO_SIGNAL)
+
+
+#define RC522_SDA        6  //PD6(SS)
+#define RC522_RESET      5  //PD5(RESET)
+
+
 PS_PAD PAD(PS2_SEL);
 
 Servo servo1;
@@ -14,9 +23,14 @@ void setup()
 {
   Serial.begin(9600);
   PAD.init();
-  pinMode(8, INPUT);
+  pinMode(LEAD_SW, INPUT);
 
-  servo1.attach(9);
+  pinMode(RC522_SDA, OUTPUT);
+  digitalWrite(RC522_SDA, HIGH);
+  pinMode(RC522_RESET, OUTPUT);
+  digitalWrite(RC522_RESET, LOW);
+  
+  servo1.attach(SERVO_SIGNAL);
 }
 
 void loop() {
@@ -25,7 +39,7 @@ void loop() {
   int deg = 0;
   int lead_sw = false;
 
-  lead_sw = digitalRead(8);
+  lead_sw = digitalRead(LEAD_SW);
 
   deg = PAD.read(PS_PAD::ANALOG_RX);
   Serial.print(lead_sw);
@@ -37,13 +51,13 @@ void loop() {
 
 
   if (PAD.read(PS_PAD::PAD_CIRCLE)) {
-    servo1.attach(9);
+    servo1.attach(SERVO_SIGNAL);
   } else if (PAD.read(PS_PAD::PAD_X)) {
     servo1.detach();
   }
 
   if (lead_sw == true) {
-    servo1.attach(9);
+    servo1.attach(SERVO_SIGNAL);
     servo1.write(180);
     delay(2000);
     servo1.detach();
